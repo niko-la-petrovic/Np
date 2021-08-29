41a561a38c203e3dd18fe1f1c840b11793420383
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-08-29 10:46:23 +0200

    Reorganized project structure.

--- src/Np.Windows/Properties/AssemblyInfo.cs
+++ src/Np.Windows/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Np.Windows")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for Windows-specific DLLs and structures.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Np.Windows")]
