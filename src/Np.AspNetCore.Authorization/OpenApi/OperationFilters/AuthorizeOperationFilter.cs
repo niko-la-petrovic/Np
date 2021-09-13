f4da0df5810dcab4554f9245fd3e5caf8f46412b
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-09-13 23:12:50 +0200

    Added AuthorizeOperationFilter.

---
+++ src/Np.AspNetCore.Authorization/OpenApi/OperationFilters/AuthorizeOperationFilter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Np.AspNetCore.Authorization.OpenApi.OperationFilters
+{
+    /// <summary>
+    /// Swagger Generator filter for authorized methods
+    /// </summary>
+    public class AuthorizeCheckOperationFilter : IOperationFilter
+    {
+        /// <summary>
+        /// Adds OpenAPI responses to the current operation and context
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="context"></param>
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var hasAuthorize =
+              context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any()
+              || context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
+
+            if (hasAuthorize)
+            {
+                operation.Security = new List<OpenApiSecurityRequirement>
+                {
+                    new OpenApiSecurityRequirement
+                    {
+                        [ new OpenApiSecurityScheme{
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            },
+                            Scheme = JwtBearerDefaults.AuthenticationScheme,
+                            Name = JwtBearerDefaults.AuthenticationScheme,
+                            In = ParameterLocation.Header
+                            }]
+                        = new string[] { }
+                    }
+                };
+            }
+        }
+    }
+}
